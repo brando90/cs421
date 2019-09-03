b7ac5796c80bf100bc20c4b9dd14ba910a2e2f60
Brando Miranda <brando90@mit.edu>
2019-09-03 11:37:51 -0500

    finished local let binding for let e=exp in f(e);;

--- lecture1/lec1.ml
+++ lecture1/lec1.ml
@@ -15,6 +15,8 @@ print_bool (2 < 3);;
 print_string "\n";;
 *)
 
+Printf.printf "%B\n" (3 < 4);;
+
 print_int (15 * 2);;
 print_string "\n";;
 
@@ -42,3 +44,33 @@ print_string "\n";;
 (* sequencing expressions *)
 (print_string "Bye\n"; 25);;
 (print_string "\nhell1\n"; print_string "hello2\n");;
+(print_string "cs42\n"; print_int (2-3); print_string "ocaml\n");;
+
+(* Declarationsl Sequencing of declarations *)
+let x = 1 + 10;; (* declaration *)
+print_int x;;
+let test = 3 < 2;;
+Printf.printf "\n%B\n" test;;
+let a = 1 let b = a + 4;;
+Printf.printf "\n%i %i\n" a b;;
+
+Printf.printf "\nOld value of test = %b \n" test;;
+let test = 3.7;;
+Printf.printf "\nNew value of test  = %f \n" test;;
+
+(*
+The construct let v = e1 in e2 defines the variable v
+which is local to the expression e2.
+The expression e1 may be a function.
+*)
+Printf.printf "\nb = %i" b;;
+let e = let b = 20 in 2*b;;
+Printf.printf "\nb = %i" b;;
+Printf.printf "\ne = %i \n" e;;
+
+Printf.printf "a = %i\n" a;;
+let c =
+  let b = a + a (* b = 1 + 1 = 2 *)
+  in b * b;; (* 4 = 2x2 *)
+Printf.printf "c = %i\n" c;;
+Printf.printf "b = %i\n" b;;
