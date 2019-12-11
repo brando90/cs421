2e02d097fb51ea0be5226330b2e62a97a0da1dcf
Brando Miranda <brando90@mit.edu>
2019-12-11 17:40:12 -0600

    done

--- ml/ml6/student.ml
+++ ml/ml6/student.ml
@@ -10,7 +10,7 @@ let rec match_lookup l n =
 let rec app_exn_handler_cont env ce n =
     match ce with
     | EmptyExnContCPS -> UncaughtException n
-    |  ExnContVarCPS i ->
+    | ExnContVarCPS i ->
       (match lookup_exn_cont env i with None -> Failed
        | Some (ke',env') -> app_exn_handler_cont env' ke' n)
     | UpdateExnContCPS (handler, ce') ->
@@ -42,5 +42,57 @@ let rec app_cont_to_value env k v =
 
 
 
-
-let rec one_step_exp_cps_eval env exp_cps = raise (Failure "Not implemented yet")
+(* cps_env_entry list -> exp_cps -> step_result *)
+let rec one_step_exp_cps_eval env exp_cps =
+  match exp_cps with
+  | VarCPS (cps_cont,x) ->
+    let some_v = lookup_value env x in
+    ( match some_v with
+      | None -> UncaughtException 0
+      | Some v ->
+        app_cont_to_value env cps_cont v
+      )
+  | ConstCPS (cps_cont,const) -> (* k v env *)
+    let const_v = const_to_val const in
+    let kv = app_cont_to_value env cps_cont const_v in kv
+  | MonOpAppCPS (cps_cont,mon_op,x,exn_cont) ->
+    let some_v = lookup_value env x in
+    ( match some_v with
+      | None -> UncaughtException 0
+      | Some v ->
+        let v_exn = monOpApply mon_op v in
+        ( match v_exn with
+          | Value v -> Final v
+          | Exn n -> app_exn_handler_cont env exn_cont n
+          )
+      )
+  | BinOpAppCPS (cps_cont,bin_op,x1,x2,exn_cont) ->
+    let some_v1 = lookup_value env x1 in
+    ( match some_v1 with
+      | None -> UncaughtException 0
+      | Some v1 ->
+        let some_v2 = lookup_value env x2 in
+        ( match some_v2 with
+          | None -> UncaughtException 0
+          | Some v2 ->
+            let v_exn = binOpApply bin_op v1 v2 in
+              match v_exn with
+              | Value v -> app_cont_to_value env cps_cont v
+              | Exn n -> app_exn_handler_cont env exn_cont n
+          )
+      )
+  | IfCPS (x,exp_cps1,exp_cps2) ->
+    let some_v_b = lookup_value env x in
+    ( match some_v_b with
+      | None -> Failed
+      | Some v_b ->
+        ( match v_b with
+          | BoolVal b ->
+            let current_exp_cps = if b then exp_cps1 else exp_cps2 in
+            Intermediate ( (env), current_exp_cps )
+          | _ -> UncaughtException 0
+          )
+      )
+  | AppCPS (cps_cont,x1,x2,exn_cont) -> raise (Failure "Not yet implemented")
+  | FunCPS (cps_cont,x,cont_var,i,exp_cps1) -> raise (Failure "Not yet implemented")
+  | FixCPS (cps_cont,x1,x2,cont_var,i,exp_cps1) -> raise (Failure "Not yet implemented")
